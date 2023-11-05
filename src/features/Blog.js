f613cd24614f54c474292b914112e53308dfc60c
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-11-05 15:30:44 +0000

    mapping blogs

--- src/features/Blog.js
+++ src/features/Blog.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Blog = () => {
+    const {blogs} = useSelector((state) => state.blogR);
+    console.log(blogs);
   return (
-    <div>Blog</div>
+    <div>
+        {
+            blogs.map((blog) => {
+                const {id, title, body} = blog;
+                return(
+                    <div key={id}>
+                        <h1>{title}</h1>
+                        <div>{body}</div>
+                    </div>
+                    )
+                })
+        }
+    </div>
   )
 }
 
