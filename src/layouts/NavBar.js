baca4a2d7015a6913f6338f1a18eac327037cd22
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-11-05 15:32:28 +0000

    NavLink for smoother transition

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const NavBar = () => {
   return (
         <nav className="nav">
-            <Link to='/' className='nav_link'>Home</Link>
-            <Link to='/show-blogs' className='nav_link'>All blogs</Link>
-            <Link to='/add-blog' className='nav_link'>Create a blog</Link>
+            <NavLink to='/' className='nav_link'>Home</NavLink>
+            <NavLink to='/show-blogs' className='nav_link'>All blogs</NavLink>
+            <NavLink to='/add-blog' className='nav_link'>Create a blog</NavLink>
         </nav>
     )
 }
